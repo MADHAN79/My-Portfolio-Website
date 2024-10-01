e6d0e96ef7a3abd56d21ba52ec647b43c69b08e8
MADHAN79 <madhanvarshini3@gmail.com>
2024-10-01 15:25:43 +0530

    mobile layout of my projects enhanced

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -47,7 +47,7 @@ const Single = ({ item }) => {
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
-    <section >
+    <section>
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
